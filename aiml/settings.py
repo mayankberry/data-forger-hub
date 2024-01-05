4b478525363f283391eb191ce4ac83ccc95289a2
Mayank Berry <91383594+mayankberry@users.noreply.github.com>
2024-01-05 13:37:30 +0530

    Update settings.py

--- aiml/settings.py
+++ aiml/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1!#6zxi9x%qr%!3jhki5l(p5lt6li&w9#u6i5qtw$uf0dpbw15'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
